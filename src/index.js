c3a017cc5ec5d6eae27d7b6612047e8f3b5f4427
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-06-08 23:49:33 +0300

    add staking platform

---
+++ src/index.js
@@ -0,0 +1,18 @@
+require("file-loader?name=[name].html!./index.html");
+
+// import css files
+import "./App.scss";
+import "./css/base.css";
+import "./css/style.css";
+import "./css/util.css";
+
+// import image files
+import "./images/icons/favicon.ico";
+import "./images/diamond.png";
+import "./images/metamask-icon.png";
+import "./images/walletconnect-icon.png";
+import "./images/climacoin-logo.png";
+
+// import js files
+import "./App.js";
+import "./assets/web3.js";
